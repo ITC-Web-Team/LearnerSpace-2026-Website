41f15c686c0a357b8b9721fa15e71c6716c98c75
icodeforlife24 <shiviutkarsh@gmail.com>
2026-06-16 15:21:45 +0530

    Update route.ts

--- main/app/auth/callback/route.ts
+++ main/app/auth/callback/route.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { createSessionToken, sessionCookieName } from "@/lib/session";
 import { fetchSsoUser } from "@/lib/sso";
-console.log("CALLBACK URL:", request.url);
 export async function GET(request: NextRequest) {
   const accessId = request.nextUrl.searchParams.get("accessid");
 
