bc18bdd5cf9d82b1026b8152fa56f251ef412cd1
icodeforlife24 <shiviutkarsh@gmail.com>
2026-06-16 14:28:49 +0530

    Hardcode SSO project ID in LoginButton
    Replace the environment variable NEXT_PUBLIC_PROJECT_ID with the fixed project UUID in LoginButton so the SSO redirect uses project 7eb1de06-b2db-4a1f-8dce-6f6ea1b608ad. If dynamic configuration is needed later, restore use of the environment variable.

--- main/app/components/LoginButton.js
+++ main/app/components/LoginButton.js
@@ -4,7 +4,7 @@ const NEXT_PUBLIC_PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID;
 
 export default function LoginButton() {
   const handleLogin = () => {
-    window.location.href = `https://sso.tech-iitb.org/project/${NEXT_PUBLIC_PROJECT_ID}/ssocall/`;
+    window.location.href = `https://sso.tech-iitb.org/project/7eb1de06-b2db-4a1f-8dce-6f6ea1b608ad/ssocall/`;
   };
 
   return (
