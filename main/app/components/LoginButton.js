b77da4b0378825b64941d82715a31ccbb5bfc95d
icodeforlife24 <shiviutkarsh@gmail.com>
2026-06-16 14:30:47 +0530

    Update LoginButton.js

--- main/app/components/LoginButton.js
+++ main/app/components/LoginButton.js
@@ -4,7 +4,7 @@ const NEXT_PUBLIC_PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID;
 
 export default function LoginButton() {
   const handleLogin = () => {
-    window.location.href = `https://sso.tech-iitb.org/project/7eb1de06-b2db-4a1f-8dce-6f6ea1b608ad/ssocall/`;
+    window.location.href = `https://sso.tech-iitb.org/project/01fc2a5f-3cb7-438e-822a-3801246d2d87/ssocall/`;
   };
 
   return (
